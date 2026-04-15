2392ef0419958e5e1884c01ca5fe30cdb59ca758
Begimai <begimajturgunbaeva8@gmail.com>
2026-04-15 21:37:39 +0600

    i didnt know about cmd+s and i saved it after i added admin even thogh it should've been done bfore that

--- houses/models.py
+++ houses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 class House(models.Model):
-    name = models.CharField('Название', max_length=100)
-    price = models.IntegerField('Цена')
-    description = models.TextField('Описание')
+    name = models.CharField("Название", max_length=100)
+    price = models.IntegerField("Цена")
+    description = models.TextField("Описание")
