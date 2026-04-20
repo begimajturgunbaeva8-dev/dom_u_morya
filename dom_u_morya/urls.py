d3a14b9c7dd5f04bbaa812cc3c64c0dfff1d073f
Begimai <begimajturgunbaeva8@gmail.com>
2026-04-20 14:46:00 +0600

    Added output of houses to the main site

--- dom_u_morya/urls.py
+++ dom_u_morya/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from houses.views import houses_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("houses/", houses_list, name="houses_list"),
 ]
--- houses/views.py
+++ houses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from houses.models import House
 
-# Create your views here.
+
+def houses_list(request):
+    houses = House.objects.all()
+    for house in houses:
+        print(house.name, house.price, house.description)
+    return render(request, "houses/houses_list.html", {"houses": houses})
