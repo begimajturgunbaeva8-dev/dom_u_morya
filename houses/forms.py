c3f28f7e2965bc7135600f036aed140833192b9e
Begimai <begimajturgunbaeva8@gmail.com>
2026-04-21 18:16:41 +0600

    Added humanizer, logo, footer

--- houses/forms.py
+++ houses/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class HouseFilterForm(forms.Form):
     min_price = forms.IntegerField(required=False, label="от")
     max_price = forms.IntegerField(required=False, label="до")
-    query = forms.CharField(required=False, label="описание")
+    query = forms.CharField(required=False, label="Описание")
     ordering = forms.ChoiceField(required=False, label="Сортировка",  choices=[("name", "По алфавиту"),
                                                                                 ("price", "Дешевые сначала"),
                                                                                 ("-price", "Дорогие сначала")
