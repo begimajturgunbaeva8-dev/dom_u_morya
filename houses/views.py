16d464acce2078105e560e35328d94eb55a5b89e
Begimai <begimajturgunbaeva8@gmail.com>
2026-04-21 14:35:14 +0600

    Redirect after sending reservation

--- houses/views.py
+++ houses/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from houses.models import House
 from orders.forms import OrderForm
 
@@ -7,6 +9,7 @@ def houses_list(request):
     houses = House.objects.all()
     return render(request, "houses/houses_list.html", {"houses": houses})
 
+
 def house_detail(request, house_id):
     house = get_object_or_404(House, pk=house_id)
     form = OrderForm(request.POST or None, initial={"house": house})
@@ -14,6 +17,6 @@ def house_detail(request, house_id):
     if request.method == "POST":
         if form.is_valid():
             form.save()
-
+            return HttpResponseRedirect(reverse("house", kwargs={"house_id": house.id}))
 
     return render(request, "houses/house_detail.html", {"house": house, "form": form})
