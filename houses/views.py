e59ab97797f2a7acea57b5900300e2c9ae16c3e1
Begimai <begimajturgunbaeva8@gmail.com>
2026-04-24 00:20:59 +0600

    Fixed name

--- houses/views.py
+++ houses/views.py
@@ -7,20 +7,27 @@ from houses.forms import HousesFilterForm
 from django.db.models import Q
 from django.utils import timezone
 from reviews.models import Review  # Импортируем модель отзывов из другого приложения
-from reviews.forms import ReviewForm # Импортируем форму для создания отзыва из другого приложения
+from reviews.forms import (
+    ReviewForm,
+)  # Импортируем форму для создания отзыва из другого приложения
+
 
 def houses_list(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         formreview = ReviewForm(request.POST)
         if formreview.is_valid():
-            formreview.save() # Сохраняем отзыв в базу
-            return redirect('house_list') # Перезагружаем страницу, чтобы увидеть отзыв
+            formreview.save()  # Сохраняем отзыв в базу
+            return redirect(
+                "houses_list"
+            )  # Перезагружаем страницу, чтобы увидеть отзыв
     else:
-        formreview = ReviewForm()  
+        formreview = ReviewForm()
 
     houses = House.objects.filter(active=True)
     form = HousesFilterForm(request.GET)
-    reviews = Review.objects.all() # Достаем все отзывы для отображения на странице со списком домов
+    reviews = (
+        Review.objects.all()
+    )  # Достаем все отзывы для отображения на странице со списком домов
     if form.is_valid():
 
         if form.cleaned_data["min_price"]:
@@ -35,7 +42,11 @@ def houses_list(request):
         if form.cleaned_data["ordering"]:
             houses = houses.order_by(form.cleaned_data["ordering"])
 
-    return render(request, "houses/houses_list.html", {"houses": houses, "form": form, "reviews": reviews, "formreview": formreview})
+    return render(
+        request,
+        "houses/houses_list.html",
+        {"houses": houses, "form": form, "reviews": reviews, "formreview": formreview},
+    )
 
 
 def house_detail(request, house_id):
