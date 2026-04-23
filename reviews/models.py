7d3e9e30400df61f2d2b27d23372afdc99787694
Begimai <begimajturgunbaeva8@gmail.com>
2026-04-23 19:04:54 +0600

    Added reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class Review(models.Model):
-    author = models.CharField("Имя автора", max_length=100)
-    text = models.TextField("Текст отзыва")
-    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.CharField("Имя", max_length=100)
+    text = models.TextField("Ваш отзыв")
+    created_at = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name = "Отзывы"
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f"Отзыв от {self.author}"
